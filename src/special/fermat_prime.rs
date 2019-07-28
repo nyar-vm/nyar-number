431e881594c4e78a83bc4db73ad85790ada239e5
Aster <galaster@foxmail.com>
2019-07-28 23:27:04 +0800

    🔧 Reformat

--- src/special/fermat_prime.rs
+++ src/special/fermat_prime.rs
@@ -21,35 +21,46 @@ impl FermatNumber {
         FermatNumber { index: int }
     }
     //pub fn nth(n: isize) -> BigInt {}
-    pub fn is_fermat_prime() {
+    pub fn is_fermat_prime(&self) {
+        let index = &self.index;
         // $F_n$ is a prime，only if $3^{{{\frac  {F_{n}-1}{2}}}}\equiv -1{\pmod  {F_{n}}}$
         unimplemented!()
     }
     pub fn value(&self) -> BigInt {
-        let b = BigInt::from(2);
         let index = &self.index;
+        let b = BigInt::from(2);
         let e = power(b.clone(), index.clone());
         power(b, e) + 1
     }
 }
 
-impl<T> From<T> for FermatNumber
-where
-    BigInt: From<T>,
-{
-    fn from(i: T) -> FermatNumber {
+impl From<BigInt> for FermatNumber {
+    fn from(i: BigInt) -> FermatNumber {
+        let v = i;
+        //let f = log2(log2(v - 1));
+        FermatNumber { index: v }
+    }
+}
+impl From<i64> for FermatNumber {
+    fn from(i: i64) -> FermatNumber {
         let v = BigInt::from(i);
         //let f = log2(log2(v - 1));
         FermatNumber { index: v }
     }
 }
-
-fn known_fermat_indexes() -> Vec<BigInt> {
-    const I: &[i8] = &[0, 1, 2, 3, 4];
-    I.to_vec().iter().map(|x| BigInt::from(*x)).collect()
+impl From<&str> for FermatNumber {
+    fn from(i: &str) -> FermatNumber {
+        let v = BigInt::parse_bytes(i.as_bytes(), 10).unwrap();
+        //let f = log2(log2(v - 1));
+        FermatNumber { index: v }
+    }
 }
 
+const KNOWN_FERMAT_PRIME_INDEXES: &[i8] = &[0, 1, 2, 3, 4];
 pub fn get_fermat_primes() -> Vec<BigInt> {
-    let v = known_fermat_indexes().into_iter();
-    v.map(|x| FermatNumber::from(x.clone()).value()).collect()
+    let v = KNOWN_FERMAT_PRIME_INDEXES.to_vec();
+    v.iter()
+        .map(|x| BigInt::from(*x))
+        .map(|x| FermatNumber::from(x).value())
+        .collect()
 }
